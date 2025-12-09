014a6ee996fc230ec7125c113a98deffa214048d
Victor <victorgms0307@gmail.com>
2025-12-09 09:55:40 +0100

    ajout footer/header/home/about

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Header } from './component/header/header';
+import { Footer } from './component/footer/footer';
+import { Home } from './component/home/home';
+import { About } from './component/about/about';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Header,
+    Footer,
+    Home,
+    About
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Home } from './component/home/home'
+import { About } from './component/about/about'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: Home},
+  {path: 'about', component: About}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
